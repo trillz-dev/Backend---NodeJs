1dc5be07a9b5a2ddeca306481e7155719c7b0890
trillz-dev <timorazzy@gmail.com>
2020-05-26 19:03:58 +0100

    API tutorials

--- Bitcoin-Ticker/index.js
+++ Bitcoin-Ticker/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/', (req, res) => {
+    // console.log(req.body.crypto) 
+
+    request('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD', (error, response, body) => {
+        console.log(body)
+    });
 });
 
 app.listen(8080, () => {
