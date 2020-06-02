d07a3a3c83766ab5b73ca315e480dca21210e9af
trillz-dev <timorazzy@gmail.com>
2020-06-03 00:37:02 +0100

    Newsletter update

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -2,3 +2,38 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
+const app = express();
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+ 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+});
+
+app.post('/', (req, res) => {
+    
+    let firstName = req.body.fName
+    let lastName = req.body.lName
+    let email = req.body.email
+
+    console.log(firstName)
+    console.log(lastName)
+    console.log(email)
+
+
+});
+
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
+});
+
+
+
+
+
+
+//cd9b3c45aa0a0b442ba71420749917eb-us10
